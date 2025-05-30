febacffe63d86cf539c2771b87bd9784e5d46375
CruzPer823 <a01736214@tec.mx>
2025-05-30 09:20:50 -0600

    feat: Add CORS middleware initial config

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,4 +1,12 @@
+from fastapi import FastAPI
 from .config import settings
 from .openapi import custom_openapi
+from .middleware import setup_cors_middleware
 
-__all__ = ["settings", "custom_openapi"]
+app = FastAPI()
+
+setup_cors_middleware(app)
+
+app.openapi = lambda: custom_openapi(app)
+
+__all__ = ["settings", "custom_openapi", "app"]
